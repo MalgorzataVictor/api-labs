941e1f9880cb8558a926827fbe114e240cc4b05c
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-11-05 17:51:57 +0000

    Completed node lab 1!

--- node_lab1/greeting.js
+++ node_lab1/greeting.js
@@ -1,6 +1,9 @@
 const greeting = {
-    en: "Hello World!",
-    fr: "Bonjour!"
+    en: "Hello World!",       
+    fr: "Bonjour!",           
+    ko: "안녕하세요 세상!",     
+    pl: "Witaj świecie!",     
+    ja: "こんにちは世界！"    
 };
 
 export default greeting;
